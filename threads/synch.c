d33c75356bbcbc21c7641fb35672dd8bb04e2e8c
mukul17star <mukul18star@gmail.com>
2017-10-02 15:36:26 +0530

    t02

--- threads/synch.c
+++ threads/synch.c
@@ -79,8 +79,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_insert_ordered(&sema->waiters, &thread_current()->elem, (list_less_func *)&priority_more, NULL);
-      //list_push_back (&sema->waiters, &thread_current ()->elem);
+      //list_insert_ordered(&sema->waiters, &thread_current()->elem, (list_less_func *)&priority_more, NULL);
+      list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
   sema->value--;
